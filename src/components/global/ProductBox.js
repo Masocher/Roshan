11e0811e37ea6ad63e00cceed8ce6fef4e97284e
masocher <masochergamer@gmail.com>
2025-02-06 21:53:48 +0330

    Create product box's off style

--- src/components/global/ProductBox.js
+++ src/components/global/ProductBox.js
@@ -1,10 +1,22 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "../../styles/global/ProductBox.module.css";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
+import {
+    faAward,
+    faPercent,
+    faSackDollar,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function ProductBox({ name, price, image, id }) {
+    const [amazing, setAmazing] = useState(false);
+    const [offer, setOffer] = useState(true);
+
     return (
         <Link className={styles.product} href={`/product/${id}`}>
+            <div className={styles.amazing_box}>شگفت انگیز</div>
+
             <Image
                 className={styles.product_image}
                 src={image}
@@ -14,9 +26,31 @@ export default function ProductBox({ name, price, image, id }) {
             <div className={styles.product_informations}>
                 <div className={styles.product_name}>{name}</div>
 
-                <div className={styles.product_price}>
+                <div
+                    className={`${styles.product_price} ${
+                        offer ? styles.show : ""
+                    }`}
+                >
                     {price}
                     <div className={styles.toman}>تومان</div>
+                </div>
+
+                <div
+                    className={`${styles.new_price} ${
+                        offer ? styles.show : ""
+                    }`}
+                >
+                    <div className={styles.content}>
+                        {price}
+                        <div className={styles.toman}>تومان</div>
+                    </div>
+
+                    <div className={styles.off_percent}>
+                        <span>
+                            <FontAwesomeIcon icon={faPercent} />
+                        </span>
+                        32
+                    </div>
                 </div>
             </div>
         </Link>
